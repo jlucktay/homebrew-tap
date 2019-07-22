2fc95cb3f82ab89731276f211f53ca2fa99ae11d
goreleaserbot <goreleaser@carlosbecker.com>
2019-07-22 06:19:41 +0100

    Brew formula update for stack version v0.3.0

--- HomebrewFormula/stack.rb
+++ HomebrewFormula/stack.rb
@@ -2,14 +2,15 @@
 class Stack < Formula
   desc "A support tool for use with Terraform stacks, Azure DevOps build pipelines, and GitHub projects/repos."
   homepage "https://github.com/jlucktay/stack"
-  version "0.2.0"
+  version "0.3.0"
+  bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/jlucktay/stack/releases/download/v0.2.0/stack_0.2.0_Darwin_x86_64.tar.gz"
-    sha256 "9759c4d0d2cebf4f9ad7191572aaf8656fbe955f0fb45472ca3ee30eebd61676"
+    url "https://github.com/jlucktay/stack/releases/download/v0.3.0/stack_0.3.0_Darwin_x86_64.tar.gz"
+    sha256 "9b4ff0d84d776325589cabfe340b2f5c28a34bb2cbd7e8c6d4de4b97aba12bd8"
   elsif OS.linux?
-    url "https://github.com/jlucktay/stack/releases/download/v0.2.0/stack_0.2.0_Linux_x86_64.tar.gz"
-    sha256 "d5cd543273a497c33d661f911d1f2d6e5a410d749d0585f5780609f9713bfd31"
+    url "https://github.com/jlucktay/stack/releases/download/v0.3.0/stack_0.3.0_Linux_x86_64.tar.gz"
+    sha256 "bc16985fd5ba9112bb83881c0978260b3a632deaaf12b3f89736c4925c6bed49"
   end
 
   def install
