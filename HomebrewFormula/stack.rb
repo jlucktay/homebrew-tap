f7ecebf38e982bdae03c4d03ee1c10f9c1ebe6c2
goreleaserbot <goreleaser@carlosbecker.com>
2019-10-08 14:14:06 +0100

    Brew formula update for stack version v0.8.0

--- HomebrewFormula/stack.rb
+++ HomebrewFormula/stack.rb
@@ -2,16 +2,16 @@
 class Stack < Formula
   desc "A support tool for use with Terraform stacks, Azure DevOps build pipelines, and GitHub projects/repos."
   homepage "https://github.com/jlucktay/stack"
-  version "0.7.1"
+  version "0.8.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/jlucktay/stack/releases/download/v0.7.1/stack_0.7.1_Darwin_x86_64.tar.gz"
-    sha256 "cde28a6f23e4f567814d53a9e0a1da62c47ca682ee373a06998d6c33258ae73e"
+    url "https://github.com/jlucktay/stack/releases/download/v0.8.0/stack_0.8.0_Darwin_x86_64.tar.gz"
+    sha256 "d143e8e1c112aa0b52ec53eb5500a23d1c0a770bc233b9fbeabfe309410f7d68"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/jlucktay/stack/releases/download/v0.7.1/stack_0.7.1_Linux_x86_64.tar.gz"
-      sha256 "08065d3f6a6b85801baceb413288e5d04ca6c288680b248fbf294f70fd261019"
+      url "https://github.com/jlucktay/stack/releases/download/v0.8.0/stack_0.8.0_Linux_x86_64.tar.gz"
+      sha256 "3f33e37ea54e10fb3a934677a2e6782b51744fd8d059908f1500b96c09dbbff8"
     end
   end
 
